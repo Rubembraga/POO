129417ada4f96b19ace491d4e5dabe0dc44dc9e9
Rubem Braga <rubembragajr@gmail.com>
2022-08-24 13:52:50 -0300

    Java Init

--- src/Principal.java
+++ src/Principal.java
@@ -1,65 +1,98 @@
+// Java code for serialization and deserialization
+// of a Java object
+import java.io.*;
+
+class Emp implements Serializable {
+    private static final long serialversionUID =
+            129348938L;
+    transient int a;
+    static int b;
+    String name;
+    int age;
+
+    // Default constructor
+    public Emp(String name, int age, int a, int b)
+    {
+        this.name = name;
+        this.age = age;
+        this.a = a;
+        this.b = b;
+    }
+
+}
+
 public class Principal {
-Package Pessoa;
-class Aluno{
-    private String nome;
-    private Integer matricula;
+    public static void printdata(Emp object1)
+    {
 
-    public String get_nome(){
-        return nome;
-    }
-    public String set_nome(String nome){
-        this.nome = nome;
-        return null;
-    }
-    public Integer get_matricula(){
-        return matricula;
-    }
-    public Integer set_matricula(Integer matricula){
-        this.matricula = matricula;
-        return matricula;
-    }
-}
-class Professor{
-    private String nome;
-    private Integer cpf;
-
-    public String get_nome(){
-        return nome;
-    }
-    public String set_name(String nome) {
-        this.nome = nome;
+        System.out.println("name = " + object1.name);
+        System.out.println("age = " + object1.age);
+        System.out.println("a = " + object1.a);
+        System.out.println("b = " + object1.b);
     }
 
-    public Integer get_cpf() {
-        return cpf;
-    }
+    public static void main(String[] args)
+    {
+        Emp object = new Emp("ab", 20, 2, 1000);
+        String filename = "shubham.txt";
 
-    public Integer set_cpf(Integer cpf) {
-        this.cpf = cpf;
-    }
+        // Serialization
+        try {
 
-}
-Package Local;
-class Universidade{
-    private String nome;
-    private String endereco;
+            // Saving of object in a file
+            FileOutputStream file = new FileOutputStream
+                    (filename);
+            ObjectOutputStream out = new ObjectOutputStream
+                    (file);
 
-    public String get_nome(){
-        return nome;
-    }
-    public String set_nome(String nome){
-        this.nome = nome;
-        return null;
-    }
-    public String get_endereco(){
-        return endereco;
-    }
-    public String set_endereco(String endereco){
-        this.endereco = endereco;
-        return endereco;
-    }
+            // Method for serialization of object
+            out.writeObject(object);
 
-}
+            out.close();
+            file.close();
 
+            System.out.println("Object has been serialized\n"
+                    + "Data before Deserialization.");
+            printdata(object);
 
+            // value of static variable changed
+            object.b = 2000;
+        }
+
+        catch (IOException ex) {
+            System.out.println("IOException is caught");
+        }
+
+        object = null;
+
+        // Deserialization
+        try {
+
+            // Reading the object from a file
+            FileInputStream file = new FileInputStream
+                    (filename);
+            ObjectInputStream in = new ObjectInputStream
+                    (file);
+
+            // Method for deserialization of object
+            object = (Emp)in.readObject();
+
+            in.close();
+            file.close();
+            System.out.println("Object has been deserialized\n"
+                    + "Data after Deserialization.");
+            printdata(object);
+
+            // System.out.println("z = " + object1.z);
+        }
+
+        catch (IOException ex) {
+            System.out.println("IOException is caught");
+        }
+
+        catch (ClassNotFoundException ex) {
+            System.out.println("ClassNotFoundException" +
+                    " is caught");
+        }
+    }
 }
